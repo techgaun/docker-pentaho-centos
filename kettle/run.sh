f853654a1c412892f0704b148acad265c9e1afd8
techgaun <coolsamar207@gmail.com>
2015-03-14 23:17:08 +0545

    Added kettle/pdi docker image

---
+++ kettle/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+HOSTNAME=$(hostname)
+
+sed -i "s/server1/server-${HOSTNAME}/g" slave_dyn.xml
+
+if [ "$CARTE_USER" -a "$CARTE_PASS" ]; then
+   echo "${CARTE_USER}: $CARTE_PASS" > pwd/kettle.pwd
+fi
+
+if [ -f "./custom_script.sh" ]; then
+   . ./custom_script.sh
+fi
+
+./carte.sh slave_dyn.xml
--- pentaho_run.sh
+++ pentaho_run.sh
@@ -10,3 +10,5 @@ sleep 30
 docker run  --name=pentaho01 -d  -p 8080:8080 -e INSTALL_PLUGIN=marketplace,cdf,cda,cde,cgg,sparkl,saiku -e  DB_PORT=5432  -e DB_DB_NAME=postgres  -e DB_USERNAME=pgadmin  -e DB_HOSTNAME=$1  -e DB_PASSWORD=pgadmin. -e DB_VENDOR=postgres techgaun/pentaho:5.2
 #sleep 360 
 #docker run  --name=pentaho02 -d  -p 8081:8080 -e INSTALL_PLUGIN=marketplace,cdf,cda,cde,cgg,sparkl,saiku  -e  DB_PORT=5432  -e DB_DB_NAME=postgres  -e DB_USERNAME=pgadmin  -e DB_HOSTNAME=$1  -e DB_PASSWORD=pgadmin. -e DB_VENDOR=postgres wmarinho/pentaho
+sleep 60
+docker run --name=pdi -d -p 8181:8181 -e CARTE_USER=techgaun -e CARTE_PASS=techgaun techgaun/kettle:5.2
